a22375ffefcdc22f225b047bd67e47163b8e4bfc
Demetrio Kirodis <kirodis@github.com>
2017-06-15 23:25:08 +0200

    Add AggregateUtils.origin

--- eventstore/src/test/java/com/github/yafna/events/aggregate/PayloadUtilsTest.java
+++ eventstore/src/test/java/com/github/yafna/events/aggregate/PayloadUtilsTest.java
@@ -16,4 +16,14 @@ public class PayloadUtilsTest {
     public void typeAnonymousl() {
         Assert.assertEquals("added", PayloadUtils.eventType(EVENT.getClass()).value());
     }
+
+    @Test
+    public void originNormal() {
+        Assert.assertEquals("rabbit", PayloadUtils.origin(RabbitAdded.class));
+    }
+
+    @Test
+    public void originAnonymous() {
+        Assert.assertEquals("rabbit", PayloadUtils.origin(EVENT.getClass()));
+    }
 }
